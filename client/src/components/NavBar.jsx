edf533e4dc0e5c350f5e81d7c2cafd55141dfdc4
rikimario <mario_martinov@abv.bg>
2023-11-12 00:15:26 +0200

    add cssBaseLine and stack no nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,57 +1,60 @@
-import { Button, AppBar, Typography, Link, Toolbar, Avatar, ThemeProvider } from "@mui/material";
+import { Button, AppBar, Typography, Toolbar, Avatar, CssBaseline, Stack } from "@mui/material";
 import styles from '../Css/Nav.module.css';
 
 export default function NavBar() {
+
     return (
+        <>
+            <CssBaseline />
+            <AppBar
+                color="default"
+                elevation={0}
+                sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+            >
+                <Toolbar sx={{ flexWrap: 'wrap' }}>
+                    <Typography variant="h3" component='div' color='chocolate' sx={{ flexGrow: 1, fontWeight: 600 }}>
+                        MovieReel
+                    </Typography>
+                    <Stack direction='row' spacing={2}>
 
-        <AppBar
-            position="static"
-            color="default"
-            elevation={0}
-            sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-        >
-            <Toolbar sx={{ flexWrap: 'wrap' }}>
-                <Typography variant="h4" component='div' noWrap sx={{ flexGrow: 1 }}>
-                    <Link className={styles.logo} href="/">MovieReel</Link>
-                </Typography>
+                        <Button
+                            className={styles.navbar}
+                            variant="button"
+                            color="text.primary"
+                            href="/"
+                            sx={{ my: 1, mx: 1 }}
+                        >
+                            Home
+                        </Button>
+                        <Button
+                            className={styles.navbar}
+                            variant="button"
+                            color="text.primary"
+                            href="#"
+                            sx={{ my: 1, mx: 1 }}
+                        >
+                            About
+                        </Button>
+                        <Button
+                            className={styles.navbar}
+                            variant="button"
+                            color="text.primary"
+                            href="#"
+                            sx={{ my: 1, mx: 1 }}
+                        >
+                            Contact
+                        </Button>
+                        <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+                            Login
+                        </Button>
+                        <Button href="#" variant="contained" sx={{ my: 1, mx: 1.5 }}>
+                            Register
+                        </Button>
+                        <Avatar alt="Elon Musk" src="https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781982181284/elon-musk-9781982181284_hr.jpg" />
 
-                <nav>
-                    <Link
-                        className={styles.navbar}
-                        variant="button"
-                        color="text.primary"
-                        href="#"
-                        sx={{ my: 1, mx: 1.5 }}
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        className={styles.navbar}
-                        variant="button"
-                        color="text.primary"
-                        href="#"
-                        sx={{ my: 1, mx: 1.5 }}
-                    >
-                        About
-                    </Link>
-                    <Link
-                        className={styles.navbar}
-                        variant="button"
-                        color="text.primary"
-                        href="#"
-                        sx={{ my: 1, mx: 1.5 }}
-                    >
-                        Contact
-                    </Link>
-                </nav>
-                <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-                    Login
-                </Button>
-                <Button href="#" variant="contained" sx={{ my: 1, mx: 1.5 }}>
-                    Register
-                </Button>
-                <Avatar alt="Elon Musk" src="https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781982181284/elon-musk-9781982181284_hr.jpg" />
-            </Toolbar>
-        </AppBar>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+        </>
     );
 };
