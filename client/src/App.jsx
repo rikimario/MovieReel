f4e6f8c5081848dbd7a804d7a740df7cb1e21bcc
rikimario <mario_martinov@abv.bg>
2023-11-17 23:56:20 +0200

    commented out footer and news letter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import NavBar from './components/NavBar.jsx';
 import Heading from './components/Heading.jsx';
-import Footer from './components/Footer.jsx';
+// import Footer from './components/Footer.jsx';
 import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+import Create from './components/Create.jsx';
+import Test from './components/Test.jsx';
 // import NavTwo from './components/NavTwo.jsx';
-import NewsLetter from './components/NewsLetter.jsx';
+// import NewsLetter from './components/NewsLetter.jsx';
 
 const darkTheme = createTheme({
   palette: {
@@ -23,15 +26,14 @@ function App() {
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='create' element={<Create />} />
+        <Route path='test' element={<Test />} />
       </Routes>
 
 
       <Heading />
 
-
-
-      <NewsLetter />
-      <Footer />
     </ThemeProvider>
   )
 }
