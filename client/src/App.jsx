75fea5a5878549599a9fded6ae3c38a760575c5d
rikimario <mario_martinov@abv.bg>
2023-11-17 23:08:24 +0200

    old nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
-// import NavBar from './components/NavBar.jsx';
+import NavBar from './components/NavBar.jsx';
 import Heading from './components/Heading.jsx';
 import Footer from './components/Footer.jsx';
 import Home from './components/Home.jsx';
-import NavTwo from './components/NavTwo.jsx';
+// import NavTwo from './components/NavTwo.jsx';
 import NewsLetter from './components/NewsLetter.jsx';
 
 const darkTheme = createTheme({
@@ -19,11 +19,11 @@ function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
 
-      <NavTwo />
-      {/* <NavBar /> */}
-      {/* <Routes>
+      {/* <NavTwo /> */}
+      <NavBar />
+      <Routes>
         <Route path='/' element={<Home />} />
-      </Routes> */}
+      </Routes>
 
 
       <Heading />
