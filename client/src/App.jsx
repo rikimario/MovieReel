82d245b2443d3803e2d97277cd0676786e0f9f4c
rikimario <mario_martinov@abv.bg>
2023-11-19 01:11:58 +0200

    add top movies to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
-import NavBar from './components/NavBar.jsx';
-import Heading from './components/Heading.jsx';
-import About from './components/About.jsx';
-import Create from './components/Create.jsx';
-import Login from './components/Login.jsx';
-// import NewsLetter from './components/NewsLetter.jsx';
-import Footer from './components/Footer.jsx';
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import NavBar from "./components/NavBar.jsx";
+import Heading from "./components/Heading.jsx";
+import About from "./components/About.jsx";
+import Create from "./components/Create.jsx";
+import Login from "./components/Login.jsx";
+import Footer from "./components/Footer.jsx";
+import TopMovies from "./components/TopMovies.jsx";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'light',
+    mode: "light",
   },
 });
 
 function App() {
-
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
@@ -23,17 +22,17 @@ function App() {
       {/* <NavTwo /> */}
       <NavBar />
       <Routes>
-        <Route path='/' element={<Heading />} />
-        <Route path='about' element={<About />} />
-        <Route path='create' element={<Create />} />
-        <Route path='login' element={<Login/>} />
+        <Route path="/" element={<Heading />} />
+        <Route path="about" element={<About />} />
+        <Route path="create" element={<Create />} />
+        <Route path="login" element={<Login />} />
       </Routes>
 
-
-      <Footer/>
+      <TopMovies />
+      <Footer />
       {/* <NewsLetter/> */}
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
