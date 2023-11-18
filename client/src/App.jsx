d8e05ebe8f9f3fe2815699c064c08271cc823856
rikimario <mario_martinov@abv.bg>
2023-11-18 12:52:26 +0200

    add footer to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import NavBar from './components/NavBar.jsx';
 import Heading from './components/Heading.jsx';
-// import Footer from './components/Footer.jsx';
-import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Create from './components/Create.jsx';
 import Test from './components/Test.jsx';
 // import NavTwo from './components/NavTwo.jsx';
 // import NewsLetter from './components/NewsLetter.jsx';
+import Footer from './components/Footer.jsx';
 
 const darkTheme = createTheme({
   palette: {
@@ -25,15 +24,15 @@ function App() {
       {/* <NavTwo /> */}
       <NavBar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Heading />} />
         <Route path='about' element={<About />} />
         <Route path='create' element={<Create />} />
         <Route path='test' element={<Test />} />
       </Routes>
 
 
-      <Heading />
-
+      <Footer/>
+      {/* <NewsLetter/> */}
     </ThemeProvider>
   )
 }
