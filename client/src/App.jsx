02933afe87341e817f565bad291b48defa5306dd
rikimario <mario_martinov@abv.bg>
2023-11-18 13:11:03 +0200

    remove test and add login to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,7 @@ import NavBar from './components/NavBar.jsx';
 import Heading from './components/Heading.jsx';
 import About from './components/About.jsx';
 import Create from './components/Create.jsx';
-import Test from './components/Test.jsx';
-// import NavTwo from './components/NavTwo.jsx';
+import Login from './components/Login.jsx';
 // import NewsLetter from './components/NewsLetter.jsx';
 import Footer from './components/Footer.jsx';
 
@@ -27,7 +26,7 @@ function App() {
         <Route path='/' element={<Heading />} />
         <Route path='about' element={<About />} />
         <Route path='create' element={<Create />} />
-        <Route path='test' element={<Test />} />
+        <Route path='login' element={<Login/>} />
       </Routes>
 
 
