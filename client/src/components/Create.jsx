c342f8dd5b84b27df285db08f415ea4e1e6b105c
rikimario <mario_martinov@abv.bg>
2023-11-18 12:25:10 +0200

    add p to create

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -1,5 +1,9 @@
 export default function Create() {
     return (
+        <>
         <h1>Create</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum totam ratione sapiente porro quo tempore. Consequuntur officia ab atque et saepe architecto, quas aspernatur velit fuga distinctio, non aperiam voluptatum. Quaerat expedita, laudantium impedit repellendus dignissimos doloremque facere, consequuntur magni enim commodi accusantium esse? Ipsam voluptatibus fugit porro labore voluptatum quidem reiciendis ad cum, cumque laborum! Et atque eius tempore nisi provident fugit vel, illo incidunt laborum repudiandae dolores deserunt, unde id sed officiis asperiores, facere sequi iste consequatur debitis mollitia recusandae ea laboriosam. Laboriosam perspiciatis, expedita omnis, soluta, beatae quia iste consectetur fugiat alias ea cumque accusantium culpa. Quam.</p>
+        
+        </>
     );
 };
