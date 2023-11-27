711df1a9cc50d5daf2762ba11f14b470c8fcc4d9
rikimario <mario_martinov@abv.bg>
2023-11-27 20:37:03 +0200

    add api for popular movies

--- client/src/components/PopularMovies.jsx
+++ client/src/components/PopularMovies.jsx
@@ -7,9 +7,42 @@ import {
   Typography,
   Container,
 } from "@mui/material/";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+const tmdb =
+  "https://api.themoviedb.org/3/movie/popular?api_key=589f3d4f48689702b074a222aea6db87";
+const poster = "https://image.tmdb.org/t/p/w500";
+
 export default function PopularMovies() {
+  const [movies, setMovie] = useState({});
+
+  useEffect(() => {
+    try {
+      fetch(tmdb)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovie(data.results.slice(0, 5));
+        });
+    } catch (error) {
+      console.error("Error fetching movies from TMDB:", error);
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(baseUrl);
+  //       const data = await response.json();
+  //       setMovie(data.results);
+  //     } catch (error) {
+  //       console.error("Error fetching data from SWAPI:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
   return (
     <>
       <Typography
@@ -30,28 +63,31 @@ export default function PopularMovies() {
           pb: 20,
         }}
       >
-        <Card sx={{ minWidth: 300, mt: 2, mr: 2 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="500"
-            width="fit-content"
-            image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Ironman
-            </Typography>
-          </CardContent>
+        {movies.map((movie) => (
+          <Card sx={{ minWidth: 300, mt: 2, mr: 2 }}>
+            <CardMedia
+              component="img"
+              alt={movie.title}
+              height="500"
+              width="fit-content"
+              image={`${poster}${movie.poster_path}`}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {movie.title}
+              </Typography>
+              {/* <h3>{movies.Title}</h3> */}
+            </CardContent>
 
-          <CardActions>
-            <Button variant="contained" component={Link} to="/movie-card">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
+            <CardActions>
+              <Button variant="contained" component={Link} to="/movie-card">
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
 
-        <Card sx={{ minWidth: 300, mt: 2, mr: 2 }}>
+        {/* <Card sx={{ minWidth: 300, mt: 2, mr: 2 }}>
           <CardMedia
             component="img"
             alt="green iguana"
@@ -121,7 +157,7 @@ export default function PopularMovies() {
           <CardActions>
             <Button variant="contained">Learn More</Button>
           </CardActions>
-        </Card>
+        </Card> */}
       </Container>
     </>
   );
