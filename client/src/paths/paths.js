1137e1e4cb6ec3c2152eac7ab9f3e87bcea3055c
rikimario <mario_martinov@abv.bg>
2023-11-29 00:15:24 +0200

    create paths

---
+++ client/src/paths/paths.js
@@ -0,0 +1,11 @@
+const Path = {
+  Home: "/",
+  Login: "/login",
+  Register: "/register",
+  Logout: "/logout",
+  About: "/about",
+  MovieCard: "/movie-card",
+  MyAccount: "/my-account",
+};
+
+export default Path;
