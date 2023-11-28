e4e6409859a82438f99f44e37744d4b7ff3abbb9
rikimario <mario_martinov@abv.bg>
2023-11-28 22:15:35 +0200

    add service for top rated and coming soon movies

--- client/src/services/movieService.js
+++ client/src/services/movieService.js
@@ -2,9 +2,25 @@ import * as request from "../lib/request";
 
 const popularUrl =
   "https://api.themoviedb.org/3/movie/popular?api_key=589f3d4f48689702b074a222aea6db87";
+const topRated =
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=589f3d4f48689702b074a222aea6db87";
+const comingSoon =
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=589f3d4f48689702b074a222aea6db87";
 
 export const getPopular = async () => {
   const result = await request.get(popularUrl);
+
+  return result;
+};
+
+export const getTopRated = async () => {
+  const result = await request.get(topRated);
+
+  return result;
+};
+
+export const getComingSoon = async () => {
+  const result = await request.get(comingSoon);
 
   return result;
 };
