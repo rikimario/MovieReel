babe8601fdf7ccbb91aae68bef834118c7e76b53
rikimario <mario_martinov@abv.bg>
2023-11-28 22:14:35 +0200

    create coming soon card

---
+++ client/src/components/coming-soon/ComingSoonCard.jsx
@@ -0,0 +1,33 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+export default function ComingSoonCard({ id, title, posterUrl, poster_path }) {
+  const poster = `${posterUrl}${poster_path}`;
+
+  return (
+    <Card sx={{ minWidth: 300, mt: 2, mr: 2 }}>
+      <CardMedia
+        component="img"
+        alt={title}
+        height="500"
+        width="fit-content"
+        image={poster}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="contained">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
