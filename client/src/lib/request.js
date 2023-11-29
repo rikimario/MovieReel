457b2c5891ff8d2aa99f5f9ad62fd0faec162949
rikimario <mario_martinov@abv.bg>
2023-11-29 19:15:02 +0200

    add request service for api

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -39,6 +39,20 @@ const request = async (method, url, data) => {
   return result;
 };
 
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODlmM2Q0ZjQ4Njg5NzAyYjA3NGEyMjJhZWE2ZGI4NyIsInN1YiI6IjY1NjRjZDJhYTM1YzhlMDBlMWU2MGJjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xie3E1pxJ80S6SI5WsW0DIPt9_TTPVbKKkhYA9Pi8Tk",
+  },
+};
+
+fetch("https://api.themoviedb.org/3/authentication", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
 export const get = request.bind(null, "GET");
 export const post = request.bind(null, "POST");
 export const put = request.bind(null, "PUT");
