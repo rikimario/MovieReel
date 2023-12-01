182e86ac67b66f2e815aa7340e5aa8514479bc90
rikimario <mario_martinov@abv.bg>
2023-12-01 19:18:32 +0200

    add create and get all services

--- client/src/services/movieService.js
+++ client/src/services/movieService.js
@@ -1,35 +1,18 @@
 import * as request from "../lib/request";
 
-const popularUrl =
-  "https://api.themoviedb.org/3/movie/popular?api_key=589f3d4f48689702b074a222aea6db87";
-const topRatedUrl =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=589f3d4f48689702b074a222aea6db87";
-const comingSoonUrl =
-  "https://api.themoviedb.org/3/movie/upcoming?api_key=589f3d4f48689702b074a222aea6db87";
+const baseUrl = "http://localhost:3030/data/lists";
 
 const apiKey = "589f3d4f48689702b074a222aea6db87";
 const apiUrl = "https://api.themoviedb.org/3/movie";
 
-export const getOnePopular = async (movieId) => {
-  const result = await request.get(`${apiUrl}/${movieId}?api_key=${apiKey}`);
+export const create = async (movieList) => {
+  const result = await request.post(baseUrl, movieList);
 
   return result;
 };
 
-export const getPopular = async () => {
-  const result = await request.get(popularUrl);
-
-  return result;
-};
-
-export const getTopRated = async () => {
-  const result = await request.get(topRatedUrl);
-
-  return result;
-};
-
-export const getComingSoon = async () => {
-  const result = await request.get(comingSoonUrl);
+export const getAll = async () => {
+  const result = await request.get(baseUrl);
 
   return result;
 };
