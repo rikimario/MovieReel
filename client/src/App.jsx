21cd8ca7ce3925ecd240c3f34991700ccafc2b22
rikimario <mario_martinov@abv.bg>
2023-12-02 20:40:55 +0200

    fixing path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,22 @@ import { useState } from "react";
 import * as authServices from "./services/authService.js";
 import Path from "./paths/paths.js";
 import AuthContext from "./context/authContext.js";
+import { MovieProvider } from "./context/movieContext.jsx";
 
 import NavBar from "./components/NavBar.jsx";
-import Heading from "./components/Heading.jsx";
+import Heading from "./components/Home.jsx";
 import About from "./components/About.jsx";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import Footer from "./components/Footer.jsx";
-import MovieDetails from "./components/MovieDetails.jsx";
+import MovieDetails from "./components/movie-details/MovieDetails.jsx";
 import MyAccount from "./components/my-account/MyAccount.jsx";
 import Logout from "./components/Logout.jsx";
-import PopularMoviesPage from "./components/popular-movies/PopularMoviesPage.jsx";
-import TopRatedPage from "./components/top-rated/TopRatedPage.jsx";
-import ComingSoonPage from "./components/coming-soon/ComingSoonPage.jsx";
-import CreateList from "./components/create-list/CreateList.jsx";
-import List from "./components/create-list/List.jsx";
+import PopularMoviesPage from "./components/movies-popular/PopularMoviesPage.jsx";
+import TopRatedPage from "./components/movies-top-rated/TopRatedPage.jsx";
+import ComingSoonPage from "./components/movies-coming-soon/ComingSoonPage.jsx";
+import CreateReview from "./components/review/CreateReview.jsx";
+import Review from "./components/review/Review.jsx";
 
 function App() {
   const navigate = useNavigate();
@@ -62,25 +63,30 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={values}>
-      <NavBar />
-      <Routes>
-        <Route path={Path.Home} element={<Heading />} />
-        <Route path={Path.About} element={<About />} />
-        <Route path={Path.Login} element={<Login />} />
-        <Route path={Path.Register} element={<Register />} />
-        <Route path={Path.Logout} element={<Logout />} />
-        <Route path={Path.MyAccount} element={<MyAccount />} />
-        <Route path={Path.CreateList} element={<CreateList />} />
-        <Route path={`${Path.List}/:id`} element={<List />} />
-        <Route path={`${Path.MovieDetails}/:id`} element={<MovieDetails />} />
-        <Route path={Path.PopularMoviesPage} element={<PopularMoviesPage />} />
-        <Route path={Path.TopRatedPage} element={<TopRatedPage />} />
-        <Route path={Path.ComingSoonPage} element={<ComingSoonPage />} />
-      </Routes>
+    <MovieProvider>
+      <AuthContext.Provider value={values}>
+        <NavBar />
+        <Routes>
+          <Route path={Path.Home} element={<Heading />} />
+          <Route path={Path.About} element={<About />} />
+          <Route path={Path.Login} element={<Login />} />
+          <Route path={Path.Register} element={<Register />} />
+          <Route path={Path.Logout} element={<Logout />} />
+          <Route path={Path.MyAccount} element={<MyAccount />} />
+          <Route path={Path.CreateReview} element={<CreateReview />} />
+          <Route path={`${Path.Review}/:id`} element={<Review />} />
+          <Route path={`${Path.MovieDetails}/:id`} element={<MovieDetails />} />
+          <Route
+            path={Path.PopularMoviesPage}
+            element={<PopularMoviesPage />}
+          />
+          <Route path={Path.TopRatedPage} element={<TopRatedPage />} />
+          <Route path={Path.ComingSoonPage} element={<ComingSoonPage />} />
+        </Routes>
 
-      <Footer />
-    </AuthContext.Provider>
+        <Footer />
+      </AuthContext.Provider>
+    </MovieProvider>
   );
 }
 
