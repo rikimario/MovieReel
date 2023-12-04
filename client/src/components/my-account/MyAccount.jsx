e4da71e954d1a8991c5a5188232d90972b63ef99
rikimario <mario_martinov@abv.bg>
2023-12-04 20:41:44 +0200

    remove uneccesery code

--- client/src/components/my-account/MyAccount.jsx
+++ client/src/components/my-account/MyAccount.jsx
@@ -25,7 +25,6 @@ export default function MyAccount() {
             flexDirection: "row",
           }}
         >
-          <img src="https://miro.medium.com/max/900/1*hy3_Usq8bFoxp4fng-Lt4w.jpeg" />
           <Container sx={{ bgcolor: "white", ml: 1 }}>
             <Typography component="h1" variant="h4" sx={{ p: 2 }}>
               First name: Mario
@@ -41,95 +40,6 @@ export default function MyAccount() {
         <Divider variant="middle" sx={{ height: 20 }} />
 
         <YourReviewsMyAcc />
-
-        <Divider variant="middle" sx={{ height: 20 }} />
-
-        {/* <Container sx={{ pt: 10 }}>
-          <Typography component="h1" variant="h3">
-            My Ratings
-          </Typography>
-          <Container sx={{ display: "flex", flexDirection: "row", py: 5 }}>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-          </Container>
-        </Container>
-        <Divider variant="middle" sx={{ height: 20 }} />
-
-        <Container sx={{ pt: 10 }}>
-          <Typography component="h1" variant="h3">
-            My Lists
-          </Typography>
-          <Container sx={{ display: "flex", flexDirection: "row", py: 5 }}>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-            <Card sx={{ maxWidth: 300, p: 1 }}>
-              <CardMedia
-                component="img"
-                alt="green iguana"
-                image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-              />
-            </Card>
-          </Container>
-        </Container> */}
       </Container>
     </>
   );
