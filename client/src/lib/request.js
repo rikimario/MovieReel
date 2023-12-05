fc856e50ac5db914ab462c2cfeaf8ed110bb6fa0
rikimario <mario_martinov@abv.bg>
2023-12-05 17:30:19 +0200

    add remove request

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -56,5 +56,5 @@ fetch("https://api.themoviedb.org/3/authentication", options)
 export const get = request.bind(null, "GET");
 export const post = request.bind(null, "POST");
 export const put = request.bind(null, "PUT");
-export const del = request.bind(null, "DELETE");
+export const remove = request.bind(null, "DELETE");
 export const patch = request.bind(null, "PATCH");
