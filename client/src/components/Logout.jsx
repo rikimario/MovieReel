8c4be861061a4a77c4845ee2d07a26fd6458c25b
rikimario <mario_martinov@abv.bg>
2023-12-06 21:53:08 +0200

    update logout to navigate to home using path

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,17 +1,25 @@
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import * as authService from ".././services/authService";
-import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/authContext";
+import Path from "../paths/paths";
 
 export default function Logout() {
   const navigate = useNavigate();
   const { logoutHandler } = useContext(AuthContext);
+
   useEffect(() => {
     authService
       .logout()
-      .then(() => logoutHandler())
-      .catch(() => navigate("/"));
+      .then(() => {
+        logoutHandler();
+        navigate(Path.Home);
+      })
+      .catch(() => {
+        logoutHandler();
+        navigate(Path.Home);
+      });
   }, []);
 
   return null;
