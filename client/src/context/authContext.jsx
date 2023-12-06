d12bc9fa2e86665ddf1ae7e10406ddae2b0b4d24
rikimario <mario_martinov@abv.bg>
2023-12-06 19:19:38 +0200

    Update authContext

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,49 +1,48 @@
 import { createContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-import * as authService from "../services/authService";
-import usePersistedState from "../hooks/usePersistedState";
-// import Path from "../paths";
-
 const AuthContext = createContext();
+
+import * as authServices from "../services/authService.js";
+import Path from "../paths/paths.js";
+import usePersistedState from "../hooks/usePersistedState.js";
 
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   const [auth, setAuth] = usePersistedState("auth", {});
 
-  const loginSubmitHandler = async (values) => {
-    const result = await authService.login(values.email, values.password);
-
-    setAuth(result);
+  const registerSubmitHandler = async (values) => {
+    const result = await authServices.register(values.email, values.password);
 
     localStorage.setItem("accessToken", result.accessToken);
 
-    navigate("/");
+    setAuth(result);
+    navigate(Path.Home);
   };
 
-  const registerSubmitHandler = async (values) => {
-    const result = await authService.register(values.email, values.password);
-
-    setAuth(result);
+  const loginSubmitHandler = async (values) => {
+    const result = await authServices.login(values.email, values.password);
 
     localStorage.setItem("accessToken", result.accessToken);
 
-    navigate("/");
+    setAuth(result);
+    navigate(Path.Home);
   };
 
   const logoutHandler = () => {
     setAuth({});
     localStorage.removeItem("accessToken");
+    navigate(Path.Home);
   };
 
   const values = {
-    loginSubmitHandler,
     registerSubmitHandler,
+    loginSubmitHandler,
     logoutHandler,
     username: auth.username || auth.email,
     email: auth.email,
     userId: auth._id,
-    isAuthenticated: !!auth.accessToken,
+    isAuth: !!auth.accessToken,
   };
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
