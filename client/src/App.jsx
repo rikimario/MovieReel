d542e33aa7782acc375783b837c744a7ec053a10
rikimario <mario_martinov@abv.bg>
2023-12-06 21:54:29 +0200

    add error boundary and routh guard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Path from "./paths/paths.js";
 import { AuthProvider } from "./context/authContext.jsx";
 
+import Home from "./components/Home.jsx";
 import NavBar from "./components/NavBar.jsx";
-import Heading from "./components/Home.jsx";
 import About from "./components/About.jsx";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
@@ -19,31 +19,40 @@ import CreateReview from "./components/review/CreateReview.jsx";
 import Review from "./components/review/Review.jsx";
 import Reviews from "./components/review/Reviews.jsx";
 import Edit from "./components/edit/Edit.jsx";
+import AuthGuard from "./components/guards/AuthGuard.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 function App() {
-
   return (
-    <AuthProvider>
-      <NavBar />
-      <Routes>
-        <Route path={Path.Home} element={<Heading />} />
-        <Route path={Path.About} element={<About />} />
-        <Route path={Path.Login} element={<Login />} />
-        <Route path={Path.Register} element={<Register />} />
-        <Route path={Path.Logout} element={<Logout />} />
-        <Route path={Path.MyAccount} element={<MyAccount />} />
-        <Route path={Path.CreateReview} element={<CreateReview />} />
-        <Route path={Path.Reviews} element={<Reviews />} />
-        <Route path={`${Path.Review}/:id`} element={<Review />} />
-        <Route path={Path.Edit} element={<Edit />} />
-        <Route path={`${Path.MovieDetails}/:id`} element={<MovieDetails />} />
-        <Route path={Path.PopularMoviesPage} element={<PopularMoviesPage />} />
-        <Route path={Path.TopRatedPage} element={<TopRatedPage />} />
-        <Route path={Path.ComingSoonPage} element={<ComingSoonPage />} />
-      </Routes>
+    <ErrorBoundary>
+      <AuthProvider>
+        <NavBar />
+        <Routes>
+          <Route path={Path.Home} element={<Home />} />
+          <Route path={Path.About} element={<About />} />
+          <Route path={Path.Login} element={<Login />} />
+          <Route path={Path.Register} element={<Register />} />
+          <Route path={Path.Reviews} element={<Reviews />} />
+          <Route path={`${Path.MovieDetails}/:id`} element={<MovieDetails />} />
+          <Route
+            path={Path.PopularMoviesPage}
+            element={<PopularMoviesPage />}
+          />
+          <Route path={Path.TopRatedPage} element={<TopRatedPage />} />
+          <Route path={Path.ComingSoonPage} element={<ComingSoonPage />} />
 
-      <Footer />
-    </AuthProvider>
+          <Route element={<AuthGuard />}>
+            <Route path={Path.MyAccount} element={<MyAccount />} />
+            <Route path={Path.CreateReview} element={<CreateReview />} />
+            <Route path={`${Path.Review}/:id`} element={<Review />} />
+            <Route path={Path.Edit} element={<Edit />} />
+            <Route path={Path.Logout} element={<Logout />} />
+          </Route>
+        </Routes>
+
+        <Footer />
+      </AuthProvider>
+    </ErrorBoundary>
   );
 }
 
